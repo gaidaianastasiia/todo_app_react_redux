d9207334faab89d0faa6f8345e646687d8ebb0cc
gaidaianastasiia <haidaianastasiia0102@meta.ua>
2019-10-22 12:48:04 +0300

    Initial commit

---
+++ src/components/todo-list/todo-list.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ToDoItem from '../todo-item/todo-item';
+
+import './todo-list.css';
+
+const ToDoList = ({tasksList, removeTask, completeTask}) => (
+    <ul className="todo-list">
+        {tasksList.map(({id, text, isCompleted}) => (
+            <ToDoItem key={id} text={text} isCompleted={isCompleted} removeTask={removeTask} completeTask={completeTask} id={id}/>
+        ))}
+    </ul>
+);
+
+ToDoList.propTypes = {
+    tasksList: PropTypes.array,
+    removeTask: PropTypes.func,
+    completeTask: PropTypes.func,
+};
+
+ToDoList.defaultProps = {
+    tasksList: [],
+    removeTask: () => {},
+    completeTask: () => {},
+};
+
+export default ToDoList;
